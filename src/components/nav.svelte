90cc7281553834c95155316ee18f89fe14bf4094
Reef-Jackson <reefworkmail@gmail.com>
2023-11-02 05:53:03 +0000

    Nav looks nicer now

--- src/components/nav.svelte
+++ src/components/nav.svelte
@@ -1,7 +1,7 @@
-<div class="navbar bg-base-100 gap-10">
-  <a href="/" class="btn btn-ghost normal-case text-xl">ModernFit Gym</a>
-  <a href="/login">Login</a>
-  <a href="/register">Register</a>
-  <a href="/trainerdash">Trainer Dashboard</a>
-  <a href="/userdash">User Dashboard</a>
+<div class="navbar bg-base-100 gap-3">
+  <a href="/" class="btn btn-ghost normal-case text-xl mr-20">ModernFit Gym</a>
+  <a href="/login" class="btn btn-ghost normal-case">Login</a>
+  <a href="/register" class="btn btn-ghost normal-case">Register</a>
+  <a href="/trainerdash" class="btn btn-ghost normal-case">Trainer Dashboard</a>
+  <a href="/userdash" class="btn btn-ghost normal-case">User Dashboard</a>
 </div>
