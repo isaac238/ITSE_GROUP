ab144767edfda643b1335cc46e39c3f0969303c3
James Westhead <james_westhead@outlook.com>
2023-11-10 01:21:08 +0000

    feat: generated a random NUMBER!
    Not finished needs to add their birth year and month and check to the database

--- src/lib/server/database_handler.js
+++ src/lib/server/database_handler.js
@@ -1,6 +1,7 @@
 import pocketbase from 'pocketbase';
 import { PUBLIC_POCKETBASE_HOST } from '$env/static/public';
 
+
 let pb = new pocketbase(PUBLIC_POCKETBASE_HOST);
 pb.autoCancellation(false);
 
@@ -25,6 +26,21 @@ export default class databaseHandler {
 	}
 
 	static async register(formData) {
+
+		var random = Math.round(Math.random()*9999)
+		
+		if(random.toString().length == 1){
+			random = "000" + random.toString();
+		}
+		else if(random.toString().length == 2){
+			random = "00" + random.toString();
+		}
+		else if(random.toString().length == 3){
+			random = "0" + random.toString();
+		}
+
+		
+
 		try {
 			const data = {
 				"username": formData.get('username'),
@@ -32,7 +48,7 @@ export default class databaseHandler {
 				"emailVisibility": true,
 				"password": formData.get('password'),
 				"passwordConfirm": formData.get('confirm-password'),
-				"pin_number": 1234
+				"pin_number": random.toString()
 			};
 
 			const record = await pb.collection('users').create(data);
