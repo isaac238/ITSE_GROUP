03e21b2b1405359eba0c17e80d5b8c3de63c4046
James Westhead <james_westhead@outlook.com>
2023-11-15 13:48:18 +0000

    feat: Started to add Terms&Conditions and Privacy Policy

--- src/components/register.svelte
+++ src/components/register.svelte
@@ -2,6 +2,44 @@
 	import LabeledInput from "./LabeledInput.svelte";
 </script>
 
+
+
+<dialog id="t&cs_modal" class="modal">
+	<div class="modal-box">
+		<h3 class="font-bold text-lg text-center pb-2">Modern Fit Terms & Conditions & Privacy Policy</h3>
+		<hr/>
+		<p>The terms & conditions and privacy policy below outline what data we ("Modern Fit") collect and how we ("Modern Fit") use it. </p>
+		<p>In this we will refer to "us" and "we" as Modern Fit the company and 'you' and 'user' as the end user who pays for and uses the services that we ("Modern Fit") give access.</p>
+
+		<h4 class="font-bold text-lg pb-2">1. Data Collected </h4>
+			<p>We collect and process Personal Data for a variety of purposes, including:</p>
+			<div class="">
+				<ul class="list-disc pl-9">
+					<li>To display personal content</li>
+					<li>To send communications</li>
+					<li>To handle contact and user support requests</li>
+					<li>To bill for our services and manage our accounts (including usage and licensing compliance)</li>
+					<li>To provide and optimize the performance of our services</li>
+				</ul>
+			</div>
+			
+			; 
+			to maintain the security of Salesforce and its services;
+			to administer surveys and conduct research; and
+			to comply with our legal obligations.
+			For the list of purposes for which we Process your Personal Data, please see the full Privacy Statement
+			
+			We only collect and process your Personal Data to the extent it is necessary for fulfilling these purposes and where we can rely on a legal basis for such processing as set out in our full Privacy Statement. Where required, we will ask you for your prior consent to processing.
+
+		<div class="modal-action">
+			<form method="dialog">
+				<!-- if there is a button in form, it will close the modal -->
+				<button class="btn">Close</button>
+			</form>
+		</div>
+	</div>
+</dialog>
+
 <main class="w-screen h-screen flex items-center justify-center">
 	<form method="post" class="card w-[25%] bg-base-300 p-10 ">
 		<h1 class="text-2xl font-bold mb-8">Register</h1>
@@ -17,8 +55,7 @@
 
 			<div class="flex justify-center">
 				<input type="checkbox" checked="checked" class="checkbox checkbox-primary " />
-				<p class="pl-4">Do you agree to the <span class="text-indigo-600 hover:underline">T&Cs</span></p>
-
+				<p class="pl-4">Do you agree to the <span onclick="showModal()" class="text-indigo-600 hover:underline cursor-pointer">T&C and Privacy Policy</span></p>
 
 			</div>
 
@@ -26,17 +63,11 @@
 		</div>
 		<p>Already have an account?<a href="/login" class="text-indigo-600 hover:underline pl-2">Login!</a></p>
 	</form>
-</main>
 
-<dialog id="t&cs_modal" class="modal">
-	<div class="modal-box">
-		<h3 class="font-bold text-lg">Hello!</h3>
-		<p class="py-4">Press ESC key or click the button below to close</p>
-		<div class="modal-action">
-			<form method="dialog">
-				<!-- if there is a button in form, it will close the modal -->
-				<button class="btn">Close</button>
-			</form>
-		</div>
-	</div>
-</dialog>
+	
+<script>
+	function showModal(){
+		document.getElementById('t&cs_modal').showModal()
+	}
+</script>
+</main>
