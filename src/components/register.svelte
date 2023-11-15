40c0b8e98fca00caaf4c5c8b5bfdd8c513de8064
James Westhead <james_westhead@outlook.com>
2023-11-15 15:04:09 +0000

    update: added more policies

--- src/components/register.svelte
+++ src/components/register.svelte
@@ -54,31 +54,60 @@
 			<h3 class="font-bold text-lg pb-4 pt-4 text-center">Privacy Policy</h3>
 			<hr class="pb-3"/>
 
+			<h5 class="font-bold text-xl pb-2 pt-2">1. Data Collected</h5>
+			<p>We collect multiple pieces of data from the user ("you") and is used throughout the website this data consists of:</p>
+			<ul class="list-disc pl-9">
+				<li>Age</li>
+				<li>Date of Birth</li>
+				<li>Full Legal Name</li>
+				<li>Email</li>
+				<li>Weight</li>
+				<li>Nutritional Information</li>
+				<li>Workout Information</li>
+			</ul>
 
-**1. Information Collection:**
-   - We collect personal information provided by users during account creation and through the use of the website.
+			<h5 class="font-bold text-xl pb-2 pt-2">2. Information Collection</h5>
+			<ul class="list-disc pl-9">
+				<li>We collect personal information provided by users during account creation and through the use of the website.</li>
+			</ul>
 
-**2. Use of Information:**
-   - Personal information is used for creating and managing user accounts, processing payments, and providing personalized services.
+			<h5 class="font-bold text-xl pb-2 pt-2">3. Use Of Information</h5>
+			<ul class="list-disc pl-9">
+				<li>Personal information is used for creating and managing user accounts, processing payments, and providing personalized services.</li>
+			</ul>
 
-**3. Security:**
-   - We implement reasonable security measures to protect the confidentiality and integrity of your personal information.
+			<h5 class="font-bold text-xl pb-2 pt-2">4. Security</h5>
+			<ul class="list-disc pl-9">
+				<li>We implement reasonable security measures to protect the confidentiality and integrity of your personal information.</li>
+			</ul>
 
-**4. Third-Party Services:**
-   - The website may integrate with third-party services, and their privacy policies may apply. We are not responsible for the practices of third parties.
+			<h5 class="font-bold text-xl pb-2 pt-2">5. Third-Party Services</h5>
+			<ul class="list-disc pl-9">
+				<li>The website may integrate with third-party services, and their privacy policies may apply. We are not responsible for the practices of third parties.</li>
+			</ul>
 
-**5. Cookies:**
-   - The website may use cookies to enhance user experience. You can adjust your browser settings to refuse cookies if you prefer.
+			<h5 class="font-bold text-xl pb-2 pt-2">6. Cookies</h5>
+			<ul class="list-disc pl-9">
+				<li>The website may use cookies to enhance user experience. You can adjust your browser settings to refuse cookies if you prefer.</li>
+			</ul>
+			
+			<h5 class="font-bold text-xl pb-2 pt-2">7. Changes to Privacy Policy</h5>
+			<ul class="list-disc pl-9">
+				<li>We reserve the right to update our privacy policy. Users will be notified of any changes, and continued use of the website implies acceptance of the updated policy.</li>
+			</ul>
 
-**6. Changes to Privacy Policy:**
-   - We reserve the right to update our privacy policy. Users will be notified of any changes, and continued use of the website implies acceptance of the updated policy.
+			<h5 class="font-bold text-xl pb-2 pt-2">8. Contact Information</h5>
+			<ul class="list-disc pl-9">
+				<li>For questions or concerns regarding the terms & conditions or privacy policy, contact us at <a class="text-indigo-600 hover:underline" href="mailto:support@modernfitgyms.com" >support@modernfitgyms.com</a> .</li>
+			</ul>
 
-**7. Contact Information:**
-   - For questions or concerns regarding the terms & conditions or privacy policy, contact us at [Your Contact Information].
+			<h5 class="font-bold text-xl pb-2 pt-2">8. Information Sharing</h5>
+			<ul class="list-disc pl-9">
+				<li>Your data may be shared with a trainer if you acquire one. This is accepted in the contract between you and the trainer when you sign up for one.</li>
+			</ul>
 
 		<div class="modal-action">
 			<form method="dialog">
-				<!-- if there is a button in form, it will close the modal -->
 				<button class="btn">Close</button>
 			</form>
 		</div>
