19a787f6bfefea21d5dbb43ba630741ce2779ba6
James Westhead <james_westhead@outlook.com>
2023-11-15 13:05:46 +0000

    feat: adding framework

--- src/components/register.svelte
+++ src/components/register.svelte
@@ -14,7 +14,28 @@
 			<LabeledInput name="password" placeholder="Password" type="password" />
 			<LabeledInput name="confirm-password" placeholder="Confirm Password" type="password" />
 			<button class="btn hover:bg-green-500 bg-slate-700 text-white" type="submit">Register</button>
+
+			<div class="flex">
+				<p>Do you agree to the <span class="text-indigo-600 hover:underline pr- ">T&Cs</span></p>
+				<input type="checkbox" checked="checked" class="checkbox checkbox-primary" />
+
+			</div>
+
+
 		</div>
 		<p>Already have an account?<a href="/login" class="text-indigo-600 hover:underline pl-2">Login!</a></p>
 	</form>
 </main>
+
+<dialog id="t&cs_modal" class="modal">
+	<div class="modal-box">
+		<h3 class="font-bold text-lg">Hello!</h3>
+		<p class="py-4">Press ESC key or click the button below to close</p>
+		<div class="modal-action">
+			<form method="dialog">
+				<!-- if there is a button in form, it will close the modal -->
+				<button class="btn">Close</button>
+			</form>
+		</div>
+	</div>
+</dialog>
