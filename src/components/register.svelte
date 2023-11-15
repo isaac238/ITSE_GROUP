48e71e6836ee39fbec13a3c423e26e384aaae09a
James Westhead <james_westhead@outlook.com>
2023-11-15 14:47:53 +0000

    feat: added more terms & conditions and started privacy policy

--- src/components/register.svelte
+++ src/components/register.svelte
@@ -5,31 +5,76 @@
 
 
 <dialog id="t&cs_modal" class="modal">
-	<div class="modal-box">
-		<h3 class="font-bold text-lg text-center pb-2">Modern Fit Terms & Conditions & Privacy Policy</h3>
-		<hr/>
-		<p>The terms & conditions and privacy policy below outline what data we ("Modern Fit") collect and how we ("Modern Fit") use it. </p>
-		<p>In this we will refer to "us" and "we" as Modern Fit the company and 'you' and 'user' as the end user who pays for and uses the services that we ("Modern Fit") give access.</p>
+	<div class="modal-box relative max-w-none w-65">
+		<h3 class="font-bold text-lg text-center pb-2 pt-2">Modern Fit Terms & Conditions & Privacy Policy</h3>
+		<hr class="pb-3"/>
+		<p>These terms and conditions ("Agreement") are entered into by and between the user ("User") and Modern Fit ("Service Provider"). 
+			This Agreement governs the use of the Modern Fit website and any related services provided by the Service Provider. 
+			By accessing or using the website, the User agrees to be bound by these terms and conditions.</p>
+		
+		<h3 class="font-bold text-lg pb-4 pt-4 text-center">Terms & Conditions For Modern Fit </h3>
+		<hr class="pb-3"/>
 
-		<h4 class="font-bold text-lg pb-2">1. Data Collected </h4>
-			<p>We collect and process Personal Data for a variety of purposes, including:</p>
-			<div class="">
-				<ul class="list-disc pl-9">
-					<li>To display personal content</li>
-					<li>To send communications</li>
-					<li>To handle contact and user support requests</li>
-					<li>To bill for our services and manage our accounts (including usage and licensing compliance)</li>
-					<li>To provide and optimize the performance of our services</li>
-				</ul>
-			</div>
-			
-			; 
-			to maintain the security of Salesforce and its services;
-			to administer surveys and conduct research; and
-			to comply with our legal obligations.
-			For the list of purposes for which we Process your Personal Data, please see the full Privacy Statement
-			
-			We only collect and process your Personal Data to the extent it is necessary for fulfilling these purposes and where we can rely on a legal basis for such processing as set out in our full Privacy Statement. Where required, we will ask you for your prior consent to processing.
+			<h4 class="font-bold text-xl pb-2 pt-2">1. Acceptance & Terms</h4>
+			<ul class="list-disc pl-9">
+				<li>By accessing or using the Modern Fit website you agree to be bound by these terms and conditions. 
+					If you do not agree with any part of these terms, you must not use the website or have access to Modern Fit Gyms.</li>
+			</ul>
+			<h4 class="font-bold text-xl pb-2 pt-2">2. User Accounts</h4>
+			<ul class="list-disc pl-9">
+				<li>To access certain features of the website, you may need to create a user account.</li>
+				<li>You are responsible for maintaining the confidentiality of your account information.</li>
+				<li>You are responsible for all activities that occur under your account.</li>
+			</ul>
+			<h4 class="font-bold text-xl pb-2 pt-2">3. Use Of The Website</h4>
+			<h5 class="font-bold text-m pb-2 pt-2">Normal Users</h5>
+			<ul class="list-disc pl-9">
+				<li>The website is for personal use only. Commercial use is strictly prohibited.</li>
+				<li>Any disruptive or harmful behavior affecting the functionality of the website is not allowed.</li>
+			</ul>
+			<h5 class="font-bold text-m pb-2 pt-2">Trainers</h5>
+			<ul class="list-disc pl-9">
+				<li>The website is for personal use only and you as a coach connected to Modern Fit Gym any outside coaching is strictly prohibited.</li>
+				<li>Any disruptive or harmful behavior affecting the functionality of the website is not allowed.</li>
+				<li>Leaking data of clients is also strictly prohibited.</li>
+			</ul>
+			<h5 class="font-bold text-xl pb-2 pt-2">4. Membership & Payments</h5>
+			<ul class="list-disc pl-9">
+				<li>Membership details, fees, and payment terms are outlined on the website.</li>
+				<li>Membership pricing may change and varies between packages.</li>
+				<li>You agree to pay all fees associated with your membership promptly. Failure to do so your membership with Modern Fit and their services are suspended immediately, if not paid the debts, legal action will be pursued and you the user will be denied access permanently.</li>
+			</ul>
+			<h5 class="font-bold text-xl pb-2 pt-2">5. Code of Conduct</h5>
+			<ul class="list-disc pl-9">
+				<li>Users must adhere to a code of conduct that includes refraining from unlawful, threatening, abusive, or harassing behavior.</li>
+				<li>Uploading or distributing inappropriate content is strictly prohibited.</li>
+				<li>Termination of the contract can be made if code of conduct is broken in Modern Fit facilities too.</li>
+			</ul>
+
+			<h3 class="font-bold text-lg pb-4 pt-4 text-center">Privacy Policy</h3>
+			<hr class="pb-3"/>
+
+
+**1. Information Collection:**
+   - We collect personal information provided by users during account creation and through the use of the website.
+
+**2. Use of Information:**
+   - Personal information is used for creating and managing user accounts, processing payments, and providing personalized services.
+
+**3. Security:**
+   - We implement reasonable security measures to protect the confidentiality and integrity of your personal information.
+
+**4. Third-Party Services:**
+   - The website may integrate with third-party services, and their privacy policies may apply. We are not responsible for the practices of third parties.
+
+**5. Cookies:**
+   - The website may use cookies to enhance user experience. You can adjust your browser settings to refuse cookies if you prefer.
+
+**6. Changes to Privacy Policy:**
+   - We reserve the right to update our privacy policy. Users will be notified of any changes, and continued use of the website implies acceptance of the updated policy.
+
+**7. Contact Information:**
+   - For questions or concerns regarding the terms & conditions or privacy policy, contact us at [Your Contact Information].
 
 		<div class="modal-action">
 			<form method="dialog">
