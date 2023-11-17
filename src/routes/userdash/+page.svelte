9cdab9597c19c34b12d9bdbc18c5b3c9beed508b
Reef-Jackson <reefworkmail@gmail.com>
2023-11-17 21:17:01 +0000

    Added Workout Mobile First Dashboard UI

--- src/routes/userdash/+page.svelte
+++ src/routes/userdash/+page.svelte
@@ -1,67 +1,64 @@
 <script>
-	import Nav from '../../components/nav.svelte';
-    import DashItem from '../../components/dashitem.svelte';
-	import 'iconify-icon';
+	import { writable } from 'svelte/store';
+	import Workout from '$lib/assets/workout.jpg';
 
-	export let data;
-	const user = data.user;
-	console.log(user);
+	export let data; //Importing data so page isn't static, for route guarding (DO NOT DELETE)..
 
-    let items = [
-    {
-      title: 'Item 1',
-      content: 'This is the content for Item 1.',
-    },
-    {
-      title: 'Item 2',
-      content: 'This is the content for Item 2.',
-    },
-    {
-      title: 'Item 3',
-      content: 'This is the content for Item 3.',
-    },
-    {
-        title: 'Item 4',
-        content: 'This is the content for Item 4',
-    },
-    {
-        title: 'Item 4',
-        content: 'This is the content for Item 4',
-    },
-  ];
+	const navState = writable("Dashboard");
+	
+	function updateNavState(state) { //Sets nav state for single page navigation
+		navState.set(state);
+	}
+
 </script>
 
-<Nav/>
-<div class="relative w-screen h-screen bg-neutral-200"> <!-- Set the width and height of the parent div -->
-    <div class="absolute inset-0 m-5 rounded-xl flex gap-4">
-        <div class="w-1/6 rounded-xl p-10 bg-neutral-100 shadow-lg flex flex-col justify-between">
-            <ul>
-              <li class="pb-10"><a href="/userdash" class="text-purple-500">Dashboard</a></li>
-              <li class="pb-10"><a href="/workoutplanner" class="hover:text-purple-500 text-neutral-800">Workout Plans</a></li>
-            </ul>
-            <ul class="self-end w-[100%]">
-                <li>
-                    <a href="/logout" class="text-neutral-800 flex items-center hover:text-red-400">
-                      <iconify-icon icon="mdi:logout"/>
-                      <span class="ml-2">Logout</span>
-                    </a>
-                  </li>
-            </ul>
-          </div>
-          
-          <div class="w-5/6 bg-neutral-100 rounded-xl shadow-lg flex flex-wrap justify-items-start">
-            <div class="w-[100%] rounded-xl flex flex-wrap h-fit gap-10 m-10">
-                {#each items as item}
-                    <DashItem>
-                        <h1 class="text-white">{item.title}</h1>
-                        <p class="text-white">{item.content}</p>
-                    </DashItem>
-                {/each}
-            </div>
-            
-        </div>
-    </div>
-  </div>
-  
-<style>
-</style>
+{#if $navState == "Workout"}
+	<div class="h-fit w-screen flex-row justify-center items-center">
+		<div class="card w-[90%] h-fit p-4 bg-base-200 shadow-xl m-auto my-10">
+		  <div class="card-body">
+			<h2 class="card-title pb-5">Upper Body Workout</h2>
+			<figure><img src={Workout} class="rounded-xl h-full" alt="Shoes" /></figure>
+			<ul>
+				<li class="my-4">2x20 Chest Flys</li>
+				<li class="my-4">5x10 Dumbbell Curls</li>
+				<li class="my-4">1x10 Barbell Thingies</li>
+			</ul>
+			<div class="card-actions justify-end">
+				<button class="btn btn-primary w-full">Check it out</button>
+			</div>
+		  </div>
+		</div>
+	</div>
+	<div class="card w-[90%] h-fit p-4 bg-base-200 shadow-xl m-auto my-10">
+	  <div class="card-body">
+		<h2 class="card-title pb-5">Upper Body Workout</h2>
+		<figure><img src={Workout} class="rounded-xl h-full" alt="Shoes" /></figure>
+		<ul>
+			<li class="my-4">2x20 Chest Flys</li>
+			<li class="my-4">5x10 Dumbbell Curls</li>
+			<li class="my-4">1x10 Barbell Thingies</li>
+		</ul>
+		<div class="card-actions justify-end">
+			<button class="btn btn-primary w-full">Check it out</button>
+		</div>
+	  </div>
+	</div>
+{/if}
+
+
+
+
+<div class="btm-nav">
+	<button class="bg-red-600 text-pink-200" class:active={$navState == "Workout"} on:click={() => updateNavState("Workout")}>
+		<svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" /></svg>
+		<span class="btm-nav-label">Workout</span>
+	</button>
+	<button class="bg-blue-600 text-blue-200" class:active={$navState == "Nutrition"} on:click={() => updateNavState("Nutrition")}>
+		<svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
+		<span class="btm-nav-label">Nutrition</span>
+	</button>
+	<button class="bg-teal-600 text-teal-200" class:active={$navState == "Dashboard"} on:click={() => updateNavState("Dashboard")}>
+		<svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z" /></svg>
+		<span class="btm-nav-label">Dashboard</span>
+	</button>
+</div>	
