dae29e3722ee50f2356db3521baced94217fc749
James Westhead <james_westhead@outlook.com>
2023-11-22 12:05:42 +0000

    FEAT: Adds custom errors for passwords matching and date of birth range

--- src/lib/server/error.js
+++ src/lib/server/error.js
@@ -14,18 +14,29 @@ export class PasswordValidation extends Error{
 
             break;
             case "digits":
-                super(`\x1b[31m${"Password does not contain numbers!"} \x1b[0m `)
+                this.message = `\x1b[31m${"Password does not contain numbers!"} \x1b[0m `
                 this.name = "PasswordValidationNoNumbers"
                 this.type = "digits"
 
 
             break;
             case "characters":
-                super(`\x1b[31m${"Password does not contain any of these special characters !@#$%^&*:?.,- !"} \x1b[0m `)
+                this.message = (`\x1b[31m${"Password does not contain any of these special characters !@#$%^&*:?.,- !"} \x1b[0m `)
                 this.name = "PasswordValidationNoSpecialCharacters"
                 this.type = "characters"
 
             break;
         }
     }
+}
+
+export class PasswordsNotMatching extends Error{
+    constructor(){
+        this.message = "Passwords do not match" 
+        this.name = "PasswordsNotMatching"
+    }
+}
+
+export class BirthdayOutOfRange extends Error{
+
 }
