3d5ad5cfc79efb716e3ad97697b847a1893608eb
Reef-Jackson <reefworkmail@gmail.com>
2023-11-28 05:07:51 +0000

    Items are now components

---
+++ src/components/MobileItem.svelte
@@ -0,0 +1,17 @@
+<script>
+
+	export let title;
+	export let subtitle;
+	export let description;
+
+</script>
+
+
+<div class="snap-always snap-center text-white flex-shrink-0 w-[100%] h-100% bg-gradient-to-r from-amber-500 to-red-500 rounded-lg flex justify-center items-center flex-col">
+	<div class="mx-14">
+		<h1 class="text-3xl font-bold mb-10">{title}</h1>
+		<h1 class="w-fit">{subtitle}</h1>
+		<h1 class="w-fit">{description}</h1>
+		<button class="btn btn-default w-full bg-white text-black border-none hover:bg-black hover:text-white mt-4">View</button>
+	</div>
+</div>
--- src/routes/userdash/+page.svelte
+++ src/routes/userdash/+page.svelte
@@ -1,8 +1,8 @@
 <script>
 	import { writable } from 'svelte/store';
 	import Nav from '../../components/nav.svelte';
-	import Workout from '$lib/assets/workout.jpg';
-	import WorkoutItem from '../../components/WorkoutItem.svelte';
+	import MobileItem from '../../components/MobileItem.svelte';
+	import DesktopItem from '../../components/DesktopItem.svelte';
 
 	export let data; //Importing data so page isn't static, for route guarding (DO NOT DELETE)..
 
@@ -14,8 +14,6 @@
 
 </script>
 
-
-
 <div class="w-screen h-screen flex flex-col items-center bg-transparent">
   <ul class="menu menu-horizontal rounded-xl my-2 p-0 h-fit">
     <button class="btn-sm bg-slate-800 rounded-lg mx-1">Nutrition</button>
@@ -23,48 +21,30 @@
     <button class="btn-sm bg-slate-800 rounded-lg mx-1">Dashboard</button>
   </ul>
 
-  <div class="md:hidden flex snap-x snap-mandatory overflow-x-auto p-4 space-x-4 bg-neutral w-screen flex-grow">
-    <div class="snap-always snap-center flex-shrink-0 w-[100%] h-100% bg-red-200 rounded-lg"></div>
-    <div class="snap-always snap-center flex-shrink-0 w-[100%] h-100% bg-gray-300 rounded-lg"></div>
-    <div class="snap-always snap-center flex-shrink-0 w-[100%] h-100% bg-gray-300 rounded-lg"></div>
-    <div class="snap-always snap-center flex-shrink-0 w-[100%] h-100% bg-gray-300 rounded-lg"></div>
-</div>
+
+	<div class="md:hidden flex snap-x snap-mandatory overflow-x-auto p-4 space-x-4 bg-neutral w-screen flex-grow">
+		
+		<MobileItem title="Component Title" subtitle="Subtitle" description="description" />
+
+	<!--
+		Implement component rendering as such: 
+
+		Replace the workout item with items received from the database. Adjust fields accordingly.
+
+		{#each workout as item}
+			<MobileItem title={item.title} subtitle={item.subtitle} description={item.description} />
+		{/each} -->
+
+	</div>
 
  <div class="hidden md:flex flex-wrap items-center w-4/6 h-5/6 rounded-xl overflow-y-auto gap-7 p-4">
-	<div class="w-[24rem] h-[30rem] bg-gradient-to-r from-cyan-500 to-blue-500 rounded-lg text-white p-10 text-xl flex flex-col justify-between">
-		<div>
-			<h1 class="text-3xl font-bold mb-10">Jimbo 10k</h1>
-			<h1>The Jimbo Special</h1>
-			<h1>Alcohol for preworkout.</h1>
-		</div>
-		<button class="btn btn-default bg-white text-black border-none hover:bg-black hover:text-white mt-4">View</button>
-	</div>
-	<div class="w-[24rem] h-[30rem] bg-gradient-to-r from-amber-500 to-red-500 rounded-lg text-white p-10 text-xl flex flex-col justify-between">
-		<div>
-			<h1 class="text-3xl font-bold mb-10">Jimbo 10k</h1>
-			<h1>The Jimbo Special</h1>
-			<h1>Alcohol for preworkout.</h1>
-		</div>
-		<button class="btn btn-default bg-white text-black border-none hover:bg-black hover:text-white mt-4">View</button>
-	</div>
-	<div class="w-[24rem] h-[30rem] bg-gradient-to-r from-cyan-500 to-blue-500 rounded-lg text-white p-10 text-xl flex flex-col justify-between">
-		<div>
-			<h1 class="text-3xl font-bold mb-10">Jimbo 10k</h1>
-			<h1>The Jimbo Special</h1>
-			<h1>Alcohol for preworkout.</h1>
-		</div>
-		<button class="btn btn-default bg-white text-black border-none hover:bg-black hover:text-white mt-4">View</button>
-	</div>
-	<div class="w-[24rem] h-[30rem] bg-gradient-to-r from-amber-500 to-red-500 rounded-lg text-white p-10 text-xl flex flex-col justify-between">
-		<div>
-			<h1 class="text-3xl font-bold mb-10">Jimbo 10k</h1>
-			<h1>The Jimbo Special</h1>
-			<h1>Alcohol for preworkout.</h1>
-		</div>
-		<button class="btn btn-default bg-white text-black border-none hover:bg-black hover:text-white mt-4">View</button>
-	</div>
-	
 
+ <!-- Same implementation here, just feed the values in from the same source, will populate both UIs. -->
+	<DesktopItem title="Title" subtitle="Subtitle" description="Description" />
+	<DesktopItem title="Title" subtitle="Subtitle" description="Description" />
+	<DesktopItem title="Title" subtitle="Subtitle" description="Description" />
+	<DesktopItem title="Title" subtitle="Subtitle" description="Description" />
+	<DesktopItem title="Title" subtitle="Subtitle" description="Description" />
  </div>
 </div>
 
