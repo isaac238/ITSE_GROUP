59d3820ee4cdd980d10e7fc3405636fc16f4d0f0
Reef-Jackson <reefworkmail@gmail.com>
2023-11-30 06:28:30 +0000

    Created branch for the Home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,33 @@
 <script>
-    import Nav from '../components/nav.svelte';
+    import Nav from "../components/nav.svelte";
 </script>
 
 <Nav />
+
+<!-- 
+I've added a basic hero element here to get you started
+
+Feel free to use DaisyUI components to familiarize yourself with Tailwind. 
+
+Please do not use Vanilla CSS when creating this page, utility classes or tailwind @apply 's only.
+-->
+
+<div
+    class="hero min-h-screen"
+    style="background-image: url(https://daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.jpg);"
+>
+    <div class="hero-overlay bg-opacity-60" />
+    <div class="hero-content text-center text-neutral-content">
+        <div class="max-w-md text-white">
+            <h1 class="mb-5 text-5xl font-bold">Welcome to ModernFit Gym</h1>
+            <p class="mb-5">
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                Maxime, laborum.
+            </p>
+            <button
+                class="btn bg-white text-black hover:bg-black hover:text-white border-none"
+                >See what we have to offer</button
+            >
+        </div>
+    </div>
+</div>
