6ee427068ca676fe19a2de800ffcea5a6d59ea18
Isaac Sutherland <isaac@isaac238.dev>
2023-12-06 19:20:02 +0000

    Added subtitle to object passed to page

--- src/lib/server/collections.js
+++ src/lib/server/collections.js
@@ -17,6 +17,9 @@ export default class Collections {
 				const data = await this.pb.collection(collection).getFullList({
 					expand: collectionsAndExpansions[collection],
 				});
+				data.forEach(record => {
+					record.subtitle = getSubtitle(collection, record);
+				});
 				response[collection] = data;
 			}
 		} catch (error) {
@@ -24,5 +27,27 @@ export default class Collections {
 		}
 
 		return response;
+	}
+
+	async getRecord(collection, id) {
+		try {
+			const record = await this.pb.collection(collection).getOne(id);
+			return record;
+		} catch (error) {
+			console.log(error);
+			return undefined;
+		}
+	}
+}
+
+const getSubtitle = (table, record) => {
+	if (table == "workout_log" || table == "meal_log") {
+		const dateCreated = new Date(record.created);
+		return "Created on: " + dateCreated.toLocaleDateString() + " at " + dateCreated.toLocaleTimeString().substring(0, 5);
+	}
+
+	if (table == "workout_plan" || table == "meal_plan") {
+		const dateFor = new Date(record.plan_date);
+		return "Plan for: " + dateFor.toLocaleDateString();
 	}
 }
