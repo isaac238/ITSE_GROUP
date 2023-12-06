02ed65913587bf0ca7d7b6f0c5a69f15944cf417
Isaac Sutherland <isaac@isaac238.dev>
2023-12-06 20:18:24 +0000

    Removed some h-screen classes causing needless scrolling

--- src/routes/userdash/+page.svelte
+++ src/routes/userdash/+page.svelte
@@ -57,19 +57,29 @@
     <input id="my-drawer" type="checkbox" class="drawer-toggle" />
     <div class="drawer-content">
 		<h1 class="p-4 text-3xl font-bold">{$currentTable.split("_").map((x) => {return x[0].toUpperCase() + x.substring(1, x.length)}).join(" ")}</h1>
-        <div class="w-screen h-screen flex flex-col items-center justify-center bg-transparent">
+        <div class="w-screen flex flex-col items-center justify-center bg-transparent">
             <div class="flex justify-center items-center w-full">
                 {#if isMobile}
                     <!-- Mobile View -->
-                    <div class="md:hidden flex snap-x snap-mandatory overflow-x-auto p-4 space-x-4 bg-neutral w-screen h-screen" >
+                    <div class="md:hidden flex snap-x snap-mandatory overflow-x-auto p-4 space-x-4 bg-neutral w-screen" >
 					{#each collectionData as record} 
+						<MobileItem title={record.name} subtitle={record.subtitle}/>
+						<MobileItem title={record.name} subtitle={record.subtitle}/>
+						<MobileItem title={record.name} subtitle={record.subtitle}/>
+						<MobileItem title={record.name} subtitle={record.subtitle}/>
+						<MobileItem title={record.name} subtitle={record.subtitle}/>
 						<MobileItem title={record.name} subtitle={record.subtitle}/>
 					{/each}
                     </div>
                 {:else}
                     <!-- Desktop View -->
-                    <div class="flex flex-wrap items-center w-[82rem] h-[50rem] rounded-xl overflow-y-auto gap-7 p-4" >
+                    <div class="flex flex-wrap items-center w-[82rem] h-[63rem] rounded-xl overflow-y-auto gap-7 p-4" >
 					{#each collectionData as record}
+						<DesktopItem title={record.name} subtitle={record.subtitle}/>
+						<DesktopItem title={record.name} subtitle={record.subtitle}/>
+						<DesktopItem title={record.name} subtitle={record.subtitle}/>
+						<DesktopItem title={record.name} subtitle={record.subtitle}/>
+						<DesktopItem title={record.name} subtitle={record.subtitle}/>
 						<DesktopItem title={record.name} subtitle={record.subtitle}/>
 					{/each}
                     </div>
