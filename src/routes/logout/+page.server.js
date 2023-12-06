3aca7d450a73cfc07b0ee59ef65c23b08718fa6f
Isaac Sutherland <isaac@isaac238.dev>
2023-12-06 15:09:08 +0000

    Logout fixed

--- src/routes/logout/+page.server.js
+++ src/routes/logout/+page.server.js
@@ -1,8 +1,9 @@
 import  { redirect } from '@sveltejs/kit';
 import Auth from '$lib/server/auth';
 
-export function load({ locals }) {
+export async function load({ locals }) {
 	const auth = new Auth(locals.pb);
-	const response = auth.logout();
+	const response = await auth.logout();
+	console.log(response);
 	if (response.success) throw redirect(301, '/login');
 }
