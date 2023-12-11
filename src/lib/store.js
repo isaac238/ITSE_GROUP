422c9129956066e8ce19a3572f8ad7638a33057d
James Westhead <james_westhead@outlook.com>
2023-12-11 04:52:43 +0000

    FEAT: Created a success, ERROR: Adds just gets refreshed needs a work around

--- src/lib/store.js
+++ src/lib/store.js
@@ -2,17 +2,27 @@
 import { writable } from "svelte/store";
 
 // exports
-export const errorsStore = createErrorNotificationsStore()
+export const errorsStore = createErrorNotificationsStore();
+export const successStore = createSuccessNotificationsStore();
 
 // Custom error store
 function createErrorNotificationsStore(){
-    const {subscribe,set,update} = writable([])
+const {subscribe,set,update} = writable([])
 
     return {
         subscribe,
         // Add notification update method
         addNotification: (notificationMessage) => update((prevVal)=>[...prevVal,notificationMessage]),
         // Remove notification update method
+        removeNotification: (newArray) => update(()=>newArray)
+    }
+}
+
+function createSuccessNotificationsStore(){
+    const{subscribe,set,update} = writable([])
+    return{
+        subscribe,
+        addNotification:(notificationMessage)=>update((prevVal)=>[...prevVal,notificationMessage]),
         removeNotification: (newArray) => update(()=>newArray)
     }
 }
--- src/routes/register/+page.server.js
+++ src/routes/register/+page.server.js
@@ -1,6 +1,6 @@
 import { error, redirect } from "@sveltejs/kit";
 import Auth from "$lib/server/auth";
-import { errorsStore } from "../../lib/store.js";
+import { errorsStore,successStore } from "../../lib/store.js";
 
 export const actions = {
 	default: async ({ request, locals }) => {
@@ -9,8 +9,7 @@ export const actions = {
 		const response = await auth.registerMember(data);
 
 		if (response.success) {
-			// pre-made code (not tested errors yet)
-			// successStore.addNotification(response.message)
+			successStore.addNotification(response)
 			throw redirect(301, "/login");}
 		else {
 			console.log(response)
