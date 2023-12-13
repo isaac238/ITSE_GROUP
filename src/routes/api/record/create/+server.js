4137c61e07e1ac6c1ccc22add0ce42259ea21cc1
Isaac Sutherland <isaac@isaac238.dev>
2023-12-13 02:31:41 +0000

    Added check for existing user in data passed in

--- src/routes/api/record/create/+server.js
+++ src/routes/api/record/create/+server.js
@@ -3,7 +3,7 @@ export async function POST({ locals, request }) {
 	const { collection, data } = body;
 	const pb = locals.pb;
 
-	if (locals.user) {
+	if (locals.user && !data.user) {
 		data.user = locals.user.id;
 	}
 
