43bcde7a75080fde052ee97738289cdf50290ece
Isaac Sutherland <isaac@isaac238.dev>
2023-12-13 02:34:43 +0000

    Added components for cardio + weight in workout log

---
+++ src/components/CardioWorkoutLog.svelte
@@ -0,0 +1,13 @@
+<script>
+	export let workout;
+	const name = workout.exercise;
+	const duration = workout.durationInSeconds;
+	const distance = workout.distanceMiles;
+	console.log(workout);
+</script>
+
+<div class="relative w-full h-[5rem] mx-auto my-3 p-3 lg:p-0 rounded-lg bg-gradient-to-r from-blue-400 to-blue-600 text-white shadow-lg lg:shadow-none flex items-center justify-center gap-4 lg:gap-10">
+	<h1 class="text-xl lg:min-w-[7rem]">{name}</h1>
+	<h1 class="text-xl lg:min-w-[7rem]">{duration}s</h1>
+	<h1 class="text-xl lg:min-w-[7rem]">{distance} Miles</h1>
+</div>
---
+++ src/components/WeightWorkoutLog.svelte
@@ -0,0 +1,14 @@
+<script>
+	export let workout;
+	const name = workout.exercise;
+	const reps = workout.reps;
+	const sets = workout.sets;
+	const weight = workout.weight_kg;
+	console.log(workout);
+</script>
+
+<div class="relative w-full h-[5rem] mx-auto my-3 p-3 lg:p-0 rounded-lg bg-gradient-to-r from-red-400 to-red-600 text-white shadow-lg lg:shadow-none flex items-center justify-center gap-4 lg:gap-10">
+	<h1 class="text-white text-xl lg:min-w-[7rem]">{name}</h1>
+	<h1 class="text-white text-xl lg:min-w-[7rem]">{sets}x{reps}</h1>
+	<h1 class="text-white text-xl lg:min-w-[7rem]">{weight}kg</h1>
+</div>
