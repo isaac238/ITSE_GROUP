e00400399c03c1a3bce0e77d6d3f2e5b2fbb299c
Isaac Sutherland <isaac@isaac238.dev>
2023-12-13 02:33:38 +0000

    Edited workout plan to load cardio & weight workouts that can be checked off and added to a log

--- src/routes/userdash/posts/workout_plan/[plan]/+page.svelte
+++ src/routes/userdash/posts/workout_plan/[plan]/+page.svelte
@@ -1,107 +1,121 @@
 <script>
-    import { writable } from "svelte/store";
-    import { onMount } from "svelte";
+    import WeightWorkoutPlan from "../../../../../components/WeightWorkoutPlan.svelte";
+    import CardioWorkoutPlan from "../../../../../components/CardioWorkoutPlan.svelte";
+	import { goto } from "$app/navigation";
 
-    let workout;
+	export let data;
 
-    const screenWidth = writable(0);
+	const recordData = data.recordData;
+	const weight_workouts = recordData.expand.weight_workouts;
+	const cardio_workouts = recordData.expand.cardio_workouts;
 
-    onMount(() => {
-        const updateScreenWidth = () => {
-            screenWidth.set(window.innerWidth);
-        };
+	let allWeightWorkoutsCompleted;
+	let allCardioWorkoutsCompleted;
 
-        updateScreenWidth();
+	let disabled;
+	let logTip;
 
-        window.addEventListener("resize", updateScreenWidth);
+	$:  allWeightWorkoutsCompleted = weight_workouts.every((workout) => workout.complete);
+	$:  allCardioWorkoutsCompleted = cardio_workouts.every((workout) => workout.complete);
+	$: disabled = !(allWeightWorkoutsCompleted && allCardioWorkoutsCompleted);
+	$: logTip = disabled ? "You must complete all workouts before logging this plan." : `Add all workouts to ${new Date(recordData.plan_date).toLocaleDateString()}'s' log.`;
 
-        return () => {
-            window.removeEventListener("resize", updateScreenWidth);
-        };
-    });
+	const getLog = async () => {
+		console.log("Checking for existing log");
+		const response = await fetch("/api/record/search", {
+			method: "POST",
+			body: JSON.stringify({
+				"collection": "workout_log",
+				"query": `name='${new Date(recordData.plan_date).toLocaleDateString()}'`,
+			}),
+			headers: { 'Content-Type': 'application/json' },
+		});
 
-    $: isMobile = $screenWidth < 768;
+		const responseJSON = await response.json();
+		return responseJSON != null ? responseJSON : false;
+	}
+
+	const createLog = async () => {
+		console.log("Creating new log");
+		const response = await fetch("/api/record/create", {
+			method: "POST",
+			body: JSON.stringify({
+				"collection": "workout_log",
+				"data": {
+					"user": recordData.user,
+					"name": new Date(recordData.plan_date).toLocaleDateString(),
+					"weight_workouts": recordData.weight_workouts,
+					"cardio_workouts": recordData.cardio_workouts,
+				},
+			}),
+			headers: { 'Content-Type': 'application/json' },
+		});
+
+		const responseJSON = await response.json();
+		return responseJSON != undefined ? responseJSON : false;
+	}
+
+	const updateLog = async (log) => {
+		console.log("Updating existing log");
+		const response = await fetch("/api/record/update", {
+			method: "POST",
+			body: JSON.stringify({
+				"collection": "workout_log",
+				"recordID": log.id,
+				"data": {
+					"weight_workouts": [...new Set([...recordrecordData.weight_workouts, ...log.weight_workouts])],
+					"cardio_workouts": [...new Set([...recordrecordData.cardio_workouts, ...log.cardio_workouts])],
+				},
+			}),
+			headers: { 'Content-Type': 'application/json' },
+		});
+
+		const responseJSON = await response.json();
+		console.log(responseJSON);
+		return responseJSON != undefined ? responseJSON : false;
+	}
+
+	const logPlan = async () => {
+		const logAlreadyExists = await getLog();
+		console.log(logAlreadyExists);
+
+		if (!logAlreadyExists) {
+			let created = await createLog();
+			console.log(created);
+			goto("/userdash/posts/workout_log/" + created.id);
+			return;
+		} 
+
+		let updated = await updateLog(logAlreadyExists);
+		console.log(updated);
+
+		if (updated) {
+			goto("/userdash/posts/workout_log/" + updated.id);
+		}
+	}
 </script>
 
-{#if isMobile}
-    <div class="w-screen h-screen bg-slate-800">
-        <div class="w-screen h-[7vh] flex items-center justify-center">
-            <h1 class="max-w-screen text-2xl text-white font-semibold">
-                Workout Name
-            </h1>
-        </div>
+<header class="py-3 px-5">
+<h1 class="bg-transparent text-xl md:text-3xl font-black">Modern Fit <iconify-icon icon="mdi:weight-lifter"/></h1>
+<a href="/userdash"> <h2 class="bg-transparent text-lg md:text-2xl font-medium">&lt; Back</h2> </a>
+</header>
 
-        <div
-            class="w-screen h-[93vh] overflow-y-scroll bg-slate-700 flex items-center flex-col"
-        >
-            <div
-                class="relative w-[90vw] h-[5rem] my-3 p-5 mx-auto flex items-center bg-gradient-to-r from-blue-400 to-blue-700 text-white shadow-lg rounded-lg gap-4"
-            >
-                <h1 class="text-lg">Workout Name</h1>
-                <h1 class="text-lg">5x10</h1>
-                <h1 class="text-lg">40kg</h1>
-                <input
-                    type="checkbox"
-                    checked="checked"
-                    class="checkbox ml-auto"
-                />
-            </div>
+<div class="w-screen md:w-3/4 rounded-lg md:mt-14 place-self-center md:bg-slate-900 md:shadow-lg h-[5rem] flex items-center justify-center md:justify-between">
+	<h1 class="md:ml-10 max-w-screen text-2xl md:text-3xl text-white font-semibold">
+		{new Date(recordData.plan_date).toDateString() + "'s Plan"}
+	</h1>
 
-			<!-- This is the log button, it goes below all conditionals. Do not include it in any svelte conditionals.-->
+	<span class="tooltip md:mr-4 md:relative absolute bottom-0 w-screen md:w-fit" data-tip={logTip}>
+		<button on:click={() => logPlan()} class="w-11/12 h-16 my-3 md:my-0 md:h-fit md:w-fit btn btn-primary shadow-lg" disabled={disabled}>Log This Plan</button>	
+	</span>
+</div>
 
-            <button class="btn btn-primary w-[90vw] h-[4rem] my-3 shadow-lg absolute bottom-0">Log Workout</button>
+<div class="w-screen md:w-3/4 p-3 md:p-0 rounded-lg place-self-center">
+	{#each weight_workouts as workout}
+		<WeightWorkoutPlan {workout} bind:complete={workout.complete}/>
+	{/each}
 
-            <!-- {#each workout as item}
-                {#if item.type == "Weight"}
-                    <div
-                        class="w-[90vw] h-[5rem] my-3 p-5 mx-auto flex items-center bg-gradient-to-r from-blue-400 to-blue-700 text-white shadow-lg rounded-lg gap-4"
-                    >
-                        <h1 class="text-lg">{item.name}</h1>
-                        <h1 class="text-lg">{item.reps}{item.sets}</h1>
-                        <h1 class="text-lg">{item.weight}</h1>
-                        <input
-                            type="checkbox"
-                            checked="checked"
-                            class="checkbox ml-auto"
-                        />
-                    </div>
-                    <button
-                        class="btn btn-primary w-[90vw] mx-auto h-[4rem] mt-3 shadow-lg"
-                        >Log Workout</button
-                    >
-                {:else}
-                    <div
-                        class="w-[90vw] h-[5rem] my-3 p-5 mx-auto flex items-center bg-gradient-to-r from-red-400 to-red-700 text-white shadow-lg rounded-lg gap-4"
-                    >
-                        <h1 class="text-lg">{item.name}</h1>
-                        <h1 class="text-lg">{item.time}{item.sets}</h1>
-                        <h1 class="text-lg">{item.distance}</h1>
-                        <input
-                            type="checkbox"
-                            checked="checked"
-                            class="checkbox ml-auto"
-                        />
-                    </div>
-                    <button
-                        class="btn btn-primary w-[90vw] mx-auto h-[4rem] mt-3 shadow-lg"
-                        >Log Workout</button
-                    >
-                {/if}
-            {/each} -->
-        </div>
-    </div>
-{:else}
-    <div class="relative w-screen h-screen flex items-center justify-center bg-slate-800 ">
-		<div class="w-2/4 h-3/4 text-black rounded-lg overflow-y-scroll">
-			<div class="w-full h-[5rem] rounded-lg bg-slate-900 shadow-lg flex flex-row items-center justify-center">
-				<h1 class="text-white font-semibold text-3xl flex flex-row ml-10">Workout Name</h1>
-				<button class="btn btn-primary shadow-lg ml-auto mr-4">Log Workout</button>	
-			</div>
-			<div class="w-full h-[5rem] mx-auto my-3 rounded-lg bg-gradient-to-r from-blue-400 to-blue-600 flex items-center justify-center gap-10">
-				<h1 class="text-white text-xl">Dumbbell Curls</h1>
-				<h1 class="text-white text-xl">5x10</h1>
-				<h1 class="text-white text-xl">20kg</h1>
-			</div>
-		</div>
-	</div>
-{/if}
+	{#each cardio_workouts as workout}
+		<CardioWorkoutPlan {workout} bind:complete={workout.complete} />
+	{/each}
+</div>
