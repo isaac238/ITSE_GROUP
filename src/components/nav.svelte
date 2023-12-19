f85c37c21d10cb429afbf35c544c538a67a1e200
Reef-Jackson <reefworkmail@gmail.com>
2023-12-19 19:01:48 +0000

    First commit

--- src/components/nav.svelte
+++ src/components/nav.svelte
@@ -1,7 +1,21 @@
-<div class="navbar bg-base-100 gap-3">
-  <a href="/" class="btn btn-ghost normal-case text-xl mr-20">ModernFit Gym</a>
-  <a href="/login" class="btn btn-ghost normal-case">Login</a>
-  <a href="/register" class="btn btn-ghost normal-case">Register</a>
-  <a href="/trainerdash" class="btn btn-ghost normal-case">Trainer Dashboard</a>
-  <a href="/userdash" class="btn btn-ghost normal-case">User Dashboard</a>
+<div class="navbar bg-base-100">
+  <div class="navbar-start">
+    <div class="dropdown">
+      <div tabindex="0" role="button" class="btn btn-ghost btn-circle">
+        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h7" /></svg>
+      </div>
+      <ul tabindex="0" class="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+        <li><a href="/">Home</a></li>
+        <li><a href="/login">Login</a></li>
+        <li><a href="/register">Register</a></li>
+        <li><a href="/trainerdash">Trainer Dashboard</a></li>
+        <li><a href="/userdash">User Dashboard</a></li>
+      </ul>
+    </div>
+  </div>
+  <div class="navbar-center">
+    <a href="/" class="btn btn-ghost text-xl">ModernFit Gym</a>
+  </div>
+  <div class="navbar-end">
+  </div>
 </div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,10 @@
 <script>
   import "../app.postcss";
+  import Nav from "../components/nav.svelte";
 </script>
 
 <main class="bg-gradient-to-t from-gray-800 to-gray-950 flex min-h-screen flex-col">
+	<Nav />
 <slot />
 </main>
 
