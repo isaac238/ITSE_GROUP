17916d2b532e176d562b789211b66e202982833a
Isaac Sutherland <isaac@isaac238.dev>
2023-12-19 23:57:37 +0000

    Changed class string generation to conditional class

--- src/components/Notification.svelte
+++ src/components/Notification.svelte
@@ -12,15 +12,16 @@
 	// Variables
 	const type = notification.type;
 	const message = notification.message;
-	const classString = "w-[25%] alert mx-5 mt-5 shadow-md alert-" + type;
-	const icon =
-		(type == "success")
-			? "mdi:check-circle-outline"
-			: (type == "warning")
-				? "typcn:warning-outline" 
-				: (type == "error")
-					? "mdi:close-circle-outline"
-					: "";
+	const classString = "w-[25%] alert mx-5 mt-5 shadow-md";
+
+	const isSuccess = type == "success";
+	const isWarning = type == "warning";
+	const isError = type == "error";
+	let icon;
+
+	if (isError) icon = "mdi:close-circle-outline";
+	if (isWarning) icon = "typcn:warning-outline";
+	if (isSuccess) icon = "mdi:check-circle-outline";
 
 	timer();
 
@@ -28,7 +29,10 @@
 
 <!-- Render -->
 
-<div role="alert" class={classString}>
+<div role="alert" class="w-[25%] alert mx-5 mt-5 shadow-md"
+class:alert-error={isError}
+class:alert-warning={isWarning}
+class:alert-success={isSuccess}>
 	<iconify-icon class="text-2xl" icon={icon}/>
     <span>{type[0].toUpperCase() + type.substring(1, type.length)}: {message}</span>
     <button on:click={() => callback()}><iconify-icon  icon="mdi:remove-bold"/></button>
