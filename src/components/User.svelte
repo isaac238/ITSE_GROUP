8ccbd866a18e43f2e4d9bc908da49011afc3f41b
Reef-Jackson <reefworkmail@gmail.com>
2023-12-22 19:30:35 +0000

    First commit

---
+++ src/components/User.svelte
@@ -0,0 +1,8 @@
+<script>
+	export let name;
+</script>
+
+<div class="w-[97%] h-[4rem] rounded-lg mt-2 flex items-center justify-center bg-white">
+    <span class="ml-3 text-black text-lg">{name}</span>
+    <button class="ml-auto mr-2 my-1  btn btn-primary w-20">View</button>
+</div>
--- src/routes/trainerdash/+page.svelte
+++ src/routes/trainerdash/+page.svelte
@@ -1,19 +1,12 @@
 <script>
-export let data;
-const user = data.user;
-console.log(user);
+	import User from '../../components/User.svelte';
 </script>
-<div class="container">
-    Main Dash
-</div>
 
-<style>
-    .container {
-        display: flex;
-        border: solid black;
-        border-radius: 1rem;
-        margin: 1rem;
-        color: black;
-        height: 100vh;
-    }
-</style>
+<div class="h-screen w-screen flex justify-center items-center flex-col">
+	<h1 class="my-4 text-xl text-white">Client List:</h1>
+	<div class="w-[94%] md:w-1/3 h-[80%] rounded-xl flex flex-col items-center shadow-black overflow-y-auto">
+		<User name="Reef Jackson"/>
+		<User name="Reef Jackson"/>
+		<User name="Reef Jackson"/>
+	</div>
+</div>
