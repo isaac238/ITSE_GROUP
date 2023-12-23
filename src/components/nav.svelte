81063b66cc5d3b4fec185b67790e677aaeb3ed2c
Isaac Sutherland <isaac@isaac238.dev>
2023-12-23 01:31:27 +0000

    feat: Added conditionals to nav so logout shows on login and user / trainer dash only shown to their roles

--- src/components/nav.svelte
+++ src/components/nav.svelte
@@ -1,5 +1,13 @@
 <script>
+	// Imports
 	import 'iconify-icon';
+
+	// Exports
+	export let user;
+
+	// Variables
+	const isTrainer = user ? user.role.includes("trainer") : false;
+	const isMember = user ? user.role.includes("member") : false;
 </script>
 
 <div class="navbar bg-base-100">
@@ -8,12 +16,24 @@
       <div tabindex="0" role="button" class="btn btn-ghost btn-circle">
         <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h7" /></svg>
       </div>
-      <ul tabindex="0" class="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+      <ul class="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
         <li><a href="/">Home</a></li>
+
+		{#if !user}
         <li><a href="/login">Login</a></li>
         <li><a href="/register">Register</a></li>
-        <li><a href="/trainerdash">Trainer Dashboard</a></li>
-        <li><a href="/userdash">User Dashboard</a></li>
+		{/if}
+
+		{#if user}
+			<li><a href="/logout">Logout</a></li>
+			{#if isTrainer}
+				<li><a href="/trainerdash">Trainer Dashboard</a></li>
+			{/if}
+
+			{#if isMember}
+				<li><a href="/userdash">User Dashboard</a></li>
+			{/if}
+		{/if}
       </ul>
     </div>
   </div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,19 @@
 <script>
-  import "../app.postcss";
-  import Nav from "../components/nav.svelte";
+	// Imports
+	import "../app.postcss";
+
+	// Components
+	import Nav from "../components/nav.svelte";
+
+	// Exports
+	export let data;
+	let user = data.user;
+	$: user = data.user;
+	
 </script>
 
 <main class="bg-gradient-to-t from-gray-800 to-gray-950 flex min-h-screen flex-col">
-	<Nav />
+	<Nav {user}/>
 <slot />
 </main>
 
