f5693d60e691e1aaa8d066e0e3898f38c5095c91
Isaac Sutherland <isaac@isaac238.dev>
2023-12-23 16:06:41 +0000

    fix: Removed test query for trainee expansion.

--- src/routes/userdash/+page.server.js
+++ src/routes/userdash/+page.server.js
@@ -1,10 +1,6 @@
 import Collections from "$lib/server/collections";
 
 export async function load({ locals }) {
-	const x = await locals.pb.collection("users").getOne(locals.user.id,
-	 {expand: "trainees"}
-	);
-	console.log(x);
 	const collections = new Collections(locals.pb);
 	const collectionsData = await collections.getAllLogsAndPlans();
 	return { user: locals.user, collectionsData };
