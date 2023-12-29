4e0daf495d075956f694365e87d25836f8df8b43
Isaac Sutherland <isaac@isaac238.dev>
2023-12-29 21:21:50 +0000

    feat: Added mobile and desktop pin reveal to navbar

--- src/routes/+layout.server.js
+++ src/routes/+layout.server.js
@@ -1,3 +1,7 @@
- export async function load({ locals }) {
-	 return {user: locals.user}
+import Pin from '$lib/server/pin.js';
+
+export async function load({ locals }) {
+	const pin = new Pin(locals.pb);
+	const pinDecrypted = await pin.get(locals.user.pin);
+	return {user: locals.user, pin: pinDecrypted }
  }
