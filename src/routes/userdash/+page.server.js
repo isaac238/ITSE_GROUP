642f5561e5379f5471abe41620cc7481c86b7308
Isaac Sutherland <isaac@isaac238.dev>
2023-12-29 00:46:01 +0000

    fix: Userdash no longer shows items user is trainer on

--- src/routes/userdash/+page.server.js
+++ src/routes/userdash/+page.server.js
@@ -2,7 +2,7 @@ import Collections from "$lib/server/collections";
 
 export async function load({ locals }) {
 	const collections = new Collections(locals.pb);
-	const collectionsData = await collections.getAllLogsAndPlans();
+	const collectionsData = await collections.getAllLogsAndPlans(`user.id = '${locals.user.id}'`);
 	return { user: locals.user, collectionsData };
 }
 
